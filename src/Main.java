1507315ee45acf93adbc19af2961a34832a13f8f
Artem Gerasimenko <gerasimenko3452@gmail.com>
2022-09-26 10:11:25 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-    System.out.println("26 вересня, лабораторна робота №2, Герасименко А.О");
+    System.out.println("26 вересня 10 годин 10 хвилин, лабораторна робота №2, Герасименко А.О");
     }
 }
- 
